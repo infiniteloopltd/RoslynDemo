ddab4f8ee0ca7d9c84d7f9c8bf38e788c74119a1
Infinite Loop Development Ltd <info@infiniteloop.ie>
2023-01-23 19:13:17 +0100

    Sample

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace Roslyn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptOptions = ScriptOptions.Default;
+            var asmWebClient = typeof(WebClient).Assembly;
+            scriptOptions = scriptOptions.AddReferences(asmWebClient);
+            //scriptOptions = scriptOptions.AddImports("System.Net");
+            var sample = File.ReadAllText("sample.txt");
+            var result = CSharpScript.EvaluateAsync(sample, scriptOptions).Result;
+            Console.WriteLine(result);
+        }
+    }
+}
